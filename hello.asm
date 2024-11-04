f3ec97b5c9f380adf2afff7d28eabdbd1777853d
JUSHA8 <83815679+JUSHA8@users.noreply.github.com>
2024-11-04 17:13:07 +0300

    Add files via upload

---
+++ hello.asm
@@ -0,0 +1,21 @@
+SECTION .data
+	hello:	DB 'HELLO world',10
+
+	helloLen:	EQU $-hello
+
+SECTION .text
+	GLOBAL _start
+
+_start:
+	mov eax,4
+	mov ebx,1
+	mov ecx,hello
+	mov edx,helloLen
+	int 80h
+	
+	mov eax,1
+	moc ebx,0
+	int 80h
+	
+
+
